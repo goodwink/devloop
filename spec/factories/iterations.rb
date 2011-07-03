22d864db57f1034eef0180a870374048c3e06649
Kyle S. Goodwin <kyle@devscoop.com>
2011-07-03 12:26:03 -0400

    Removing versions and iterations (simplify! simplify!).

--- spec/factories/iterations.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-Factory.define :iteration do |f|
-  f.name "Sprint 1"
-  f.slug "sprint_1"
-  f.start_date "2011-06-30"
-  f.end_date "2011-07-30"
-  
-end
--- spec/factories/versions.rb
+++
@@ -1,7 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-Factory.define :version do |f|
-  f.name "1.0.0"
-  f.slug "1_0_0"
-  f.release_date "2011-07-30"
-end
